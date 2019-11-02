110b9ccd49fbf7ac2d24d12481e2b6127c196138
Jayclaw101 <johanna@cehs.net>
2019-11-02 15:16:33 +0000

    Morning 1.2

--- adventure.py
+++ adventure.py
@@ -14,14 +14,23 @@ def morning():
         else:
             print("Please enter a valid option")
             snooze = "S"
+    print()
+    print("You heave a sigh and reluctantly get out of bed. You go downstairs to get some coffee.")
+    print("As the kettle boils you absentmindedly stare at the coffee package.")
+    serialNum = input("What does the serial number say: ")
+    if serialNum == "d7b25e4c5a":
+        win()
+    else:
+        print("That's not quite right...")
+        
     option = 0
     while option not in [1,2,3,4]:
         print()
-        print("You heave a sigh and reluctantly get out of bed. Time to face the day. But what shall you do today?")
+        print("Time to face the day. But what shall you do?")
         print("You go have some breakfast as you think over your options...")
         print("(1) You could go to your morning class. I mean, you are paying to take this course after all.")
         print("(2) You have a test this afternoon. You could skip your lecture this morning and cram for it.")
-        print("(3) You could ditch them both to go to the sports centre. You haven't been eating that well this week...")
+        print("(3) You could ditch them both to go to the gym. You haven't been eating that well this week...")
         print("(4) Or you could just ditch completely. You could use a day off after all.")
         print()
         option = int(input("What should you do today: "))
@@ -48,6 +57,9 @@ def test():
     print("Not ready")
 
 def bunk():
+    print("Not ready")
+
+def win():
     print("Not ready")
 
 #Start
