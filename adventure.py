6502c83cf3ef61b3e4e61c1ce6e58a51dc3f8208
Jayclaw101 <johanna@cehs.net>
2019-11-02 14:51:06 +0000

    Morning Version 1

--- adventure.py
+++ adventure.py
@@ -1,4 +1,6 @@
 #We can make a module for each possible main path of the day
+
+#Morning module
 def morning():
     snooze = "S"
     while snooze == "S":
@@ -12,6 +14,28 @@ def morning():
         else:
             print("Please enter a valid option")
             snooze = "S"
+    option = 0
+    while option != (1,2,3,4):
+        print()
+        print("You heave a sigh and reluctantly get out of bed. Time to face the day. But what shall you do today?")
+        print("You go have some breakfast as you think over your options...")
+        print("(1) You could go to your morning class. I mean, you are paying to take this course after all.")
+        print("(2) You have a test this afternoon. You could skip your lecture this morning and cram for it.")
+        print("(3) You could ditch them both to go to the sports centre. You haven't been eating that well this week...")
+        print("(4) Or you could just ditch completely. You could use a day off after all.")
+        print()
+        option = int(input("What should you do today: "))
+        if option == 1:
+            course()
+        elif option == 2:
+            test()
+        elif option == 3:
+            sports()
+        elif option == 4:
+            bunk()
+        else:
+            print("Please input a valid option")
+    print("Sounds good.")
     
     
 def sports():
