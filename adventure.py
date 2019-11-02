1b3de07107578ad5f487cc46a4b452d52859339f
Jayclaw101 <johanna@cehs.net>
2019-11-02 14:26:29 +0000

    Snooze function

--- adventure.py
+++ adventure.py
@@ -1,3 +1,18 @@
-print("HELLO WORLD")
-print("does this work?")
-#Should do
+#We can make a module for each possible main path of the day
+def morning():
+    snooze = "S"
+    while snooze = "S"
+        print("You wake up to the sound of your alarm clock blaring.")
+        snooze = input("Do you snooze the alarm (S), or turn it off (T): ")
+        snooze = snooze.upper()
+    
+
+    
+def sports():
+
+def course():
+
+def test():
+
+def bunk():
+    
