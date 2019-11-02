eda15a931abcc463add8fb0e04ace320a963bc46
Amit <nicaami@gmail.com>
2019-11-02 22:04:50 +0000

    done pool task

--- adventure.py
+++ adventure.py
@@ -1,6 +1,8 @@
 #We can make a module for each possible main path of the day
 
 #Morning module
+import time
+from time import sleep
 def morning():
     snooze = "S"
     while snooze == "S":
@@ -9,7 +11,13 @@ def morning():
         snooze = snooze.upper()
         if snooze == "S":
             print("You hit the button and go back to sleep")
-            print("...")
+            print(".",end="")
+            sleep(0.5)
+            print(".",end="")
+            sleep(0.5)
+            print(". ",end="")
+            sleep(0.5)
+            print("\n")
         elif snooze == "T":
             print("You go into your phone and turn off the alarm.")
         else:
@@ -47,7 +55,63 @@ def morning():
             print("Please input a valid option")
     
 def sports():
-    print("You head to the gym. What do you want to do, lift some weights (W), or go to the pool (P)")
+    option = 0
+    while option not in ["W","P"]:
+        option = input("You head to the gym. What do you want to do, lift some weights (W), or go to the pool (P): ")
+        option = option.upper()
+        if not(option == "W" or option == "P"):
+            print("please select a valid option")
+    if option == "P":
+        armband = 0
+        armband = input("Do you want to wear armbands? (Y/N): ")
+        armband = armband.upper()
+        if armband == "Y":
+            print("Eww why would you want to wear armbands. You're a grown adult, you don't need them")
+        elif armband == "N":
+            print("That's not very safe!")
+        else:
+            print("I'll take that as a no. Armbands are for kids anyway")
+        sleep(1.8)    
+        print("You finally decide to jump into the pool. It's pretty empty today, except for this one old woman \nwho looks at you a bit funny and then gets in.")
+        sleep(1.8)
+        print("You start swimming, when suddenly you notice that the old woman is swimming right behind you. \nYou feel a bit nervous now")
+        sleep(1.8)
+        print("Suddenly, the old woman grabs your leg")
+        sleep(1.8)
+        kick = 0
+        start = time.time()
+        kick = input("QUICK! Press K to kick her away: ")
+        end = time.time()
+        inputTime = (end - start)
+        kick = kick.upper()
+        
+        if kick == "K" and inputTime < 1.8:
+            print("You kick her")
+        elif kick == "K":
+            sleep(0.5)
+            print("You try to kick her but you took too long to press the button, so she is able to hold on")
+            sleep(2)
+            print("Slowly, the old lady drags you down into the pool. \nYou desperately try to escape the her clutches but it's no use!")
+            sleep(2)
+            print("You're running out of breath.. you can't breathe.. YOU START TO PANIC OH GOD WHA-")
+            sleep(3)
+            print("\n\n\n\n\n\n\n\n\n\n")
+            morning()
+    elif option == "W":
+        print("hi")
+    else:
+        print("please select a valid option")   
+
+def pool():
+    option = 0
+    while option not in ["W","P"]:
+       
+        if option != "W" or "P":
+            print("please select a valid option")
+    if option == "W":
+        print("yay")
+
+
 def course():
     print("Not ready")
 
@@ -59,7 +123,6 @@ def bunk():
 
 def win():
     print("Not ready")
-
 #Start
 morning()
     
